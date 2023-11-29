b7e439f9609b46e33566363acac1c3437e12a9c4
Kadi <kadi.steam@gmail.com>
2023-11-29 12:34:07 +0100

    testing

---
+++ client/src/components/dropdown/UserProfileDropdown.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import "./UserProfileDropdown.css";
+import LogoutIcon from "../../public/images/logout.png";
+import UserSettingsIcon from "../../public/images/profile-settings.png";
+
+import { AuthContext, AuthContextType } from "../../authContext";
+
+import Usercard from "../usercard/Usercard";
+
+type User = {
+  id: number;
+  username: string;
+  imagePath: string;
+  gender: string;
+};
+
+const UserProfileDropdown: React.FC<{ User: User | undefined }> = ({
+  User,
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleToggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const { logoutHandler } = useContext(AuthContext) as AuthContextType;
+
+  return (
+    <div className="user-profile-dropdown">
+      {isOpen && (
+        <div className="dropdown-content">
+          <Link
+            className="dropdown-item-button"
+            to={{ pathname: "/edit-profile/" }}
+          >
+            <img
+              src={UserSettingsIcon}
+              className="dropdown-item-icon"
+              alt="settings"
+            />
+            <div className="dropdown-item-text">Profile Settings</div>
+          </Link>
+          <div className="dropdown-item-button" onClick={logoutHandler}>
+            <img src={LogoutIcon} className="dropdown-item-icon" alt="logout" />
+            <div className="dropdown-item-text">Logout</div>
+          </div>
+        </div>
+      )}
+      <Usercard user={User} onClick={handleToggleDropdown} />
+    </div>
+  );
+};
+
+export default UserProfileDropdown;
---
+++ client/src/components/notifications/Notifications.tsx
@@ -0,0 +1,211 @@
+import React, { useState, useEffect, useContext } from "react";
+import "./Notifications.css";
+import NotifImage from "../../public/images/notification.png";
+
+import calculateTimeAgo from "../../util/timeAgo";
+import { AuthContext, AuthContextType } from "../../authContext";
+
+type Notification = {
+  id: number;
+  type: string;
+  desc: string;
+  createdAt: string;
+  updatedAt: string;
+  notifiedId: number;
+  notifierId: number;
+  seen: boolean;
+};
+
+const Notifications: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>();
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+  const [notificationsCount, setNotificationsCount] = useState<number>(0);
+
+  const rooturl = process.env.REACT_APP_ROOT_URL as string;
+  const userId = localStorage.getItem("userId");
+  const token = localStorage.getItem("token");
+  const { socket } = useContext(AuthContext) as AuthContextType;
+
+  const handleFriendResponse = (
+    e: React.MouseEvent<HTMLDivElement>,
+    notification: Notification
+  ) => {
+    setNotifications((prevNotifs: Notification[]) => {
+      return prevNotifs.map((prevNotif: Notification) => {
+        if (prevNotif.id === notification.id) {
+          prevNotif.seen = true;
+        }
+        return prevNotif;
+      });
+    });
+
+    const reply = (e.target as HTMLDivElement).innerHTML;
+
+    fetch(rooturl + "/addFriend/" + notification.notifierId, {
+      method: "PUT",
+      body: JSON.stringify({ reply }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((resData) => {
+        if (resData.error) {
+          console.log(resData);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetch(rooturl + "/notifications/" + userId, {
+      headers: { Authorization: "Bearer " + token },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((resData) => {
+        if (resData.error) {
+          return;
+        }
+        setNotifications(resData.notifications);
+        setNotificationsCount(() => {
+          let count = 0;
+          resData.notifications.map((notif: Notification) => {
+            if (!notif.seen) {
+              count++;
+            }
+            return notif;
+          });
+          return count;
+        });
+      });
+    if (!socket) {
+      return;
+    }
+
+    socket.on("newFriendRequestNotif", (notification: Notification) => {
+      setNotifications((prevState) => {
+        return [...prevState, notification];
+      });
+      setNotificationsCount((prevState) => {
+        return prevState + 1;
+      });
+    });
+
+    socket.on("friendshipReply", (notification: Notification) => {
+      setNotifications((prevState) => {
+        return [...prevState, notification];
+      });
+      setNotificationsCount((prevState) => {
+        return prevState + 1;
+      });
+    });
+
+    return () => {
+      socket.off("newFriendRequestNotif");
+      socket.off("friendshipReply");
+    };
+  }, [isOpen, rooturl, userId, token, socket]);
+
+  const handleToggleNotifcations = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <div className="notifications-icon" onClick={handleToggleNotifcations}>
+        <img
+          className="notifications-image"
+          src={NotifImage}
+          alt="notifications"
+        />
+        {notificationsCount > 0 && (
+          <span className="notifications-count"> {notificationsCount} </span>
+        )}
+      </div>
+      {isOpen && (
+        <div className="notifications">
+          <div className="notifications-container">
+            {notifications.length > 0 ? (
+              <>
+                {notifications
+                  .map((notification: Notification) => {
+                    if (notification.type === "Friendship") {
+                      return (
+                        <div
+                          key={notification.id}
+                          className={
+                            notification.seen
+                              ? "notifications-item seen"
+                              : "notifications-item"
+                          }
+                        >
+                          <p>{notification.desc}</p>
+                          {notification.seen && <p> seen </p>}
+                          {!notification.seen && (
+                            <>
+                              <div
+                                className="notifications-button accept"
+                                onClick={(e) => {
+                                  return handleFriendResponse(e, notification);
+                                }}
+                              >
+                                Accept
+                              </div>
+                              <div
+                                className="notifications-button decline"
+                                onClick={(e) => {
+                                  return handleFriendResponse(e, notification);
+                                }}
+                              >
+                                Decline
+                              </div>
+                            </>
+                          )}
+                          <span>
+                            {calculateTimeAgo(notification.createdAt)}
+                          </span>
+                        </div>
+                      );
+                    } else if (notification.type === "FriendshipReply") {
+                      return (
+                        <div
+                          key={notification.id}
+                          className={
+                            notification.seen
+                              ? "notifications-item seen"
+                              : "notifications-item"
+                          }
+                        >
+                          <p>{notification.desc}</p>
+                          {notification.seen && <p> seen </p>}
+                          <span>
+                            {calculateTimeAgo(notification.createdAt)}
+                          </span>
+                        </div>
+                      );
+                    } else {
+                      return <div></div>;
+                    }
+                  })
+                  .reverse()}
+              </>
+            ) : (
+              <div className="notificatins-item-missing">
+                You've got no notifications yet.
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Notifications;
