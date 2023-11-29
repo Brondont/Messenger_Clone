7b14eb1221738726c383b6289c7eb7dbadee5566
Kadi <kadi.steam@gmail.com>
2023-11-29 12:23:52 +0100

    testing

--- client/src/pages/main/Home.tsx
+++ client/src/pages/main/Home.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { Socket } from "socket.io-client";
 
-import MainChat from "../../components/Mainchat/MainChat";
-import SideBar from "../../components/Sidebar/SideBar";
+import MainChat from "../../components/mainchat/MainChat";
+import SideBar from "../../components/sidebar/SideBar";
 
 type User = {
   id: number;
